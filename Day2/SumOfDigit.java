580328cb494584fe44cd5452e8501166c1598983
Bhagyashri-dhamole <bhagyashridhamole30@gmail.com>
2026-03-14 16:54:46 +0530

    Add SumOfDigit.java to calculate sum of digits

---
+++ Day2/SumOfDigit.java
@@ -0,0 +1,14 @@
+public class SumOfDigit
+{
+	public static void main(String args[])
+	{
+		int n = 1;
+		int sum = 0;
+		while(n <= 10)
+		{
+			sum = sum+n;
+			n++;
+		}
+		System.out.println(sum);
+	}
+}
